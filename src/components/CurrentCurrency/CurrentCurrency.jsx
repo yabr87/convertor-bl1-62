f4b6c9ca9e52b559cffa60db0aa587f2fcf757ff
yabr87 <108272773+yabr87@users.noreply.github.com>
2023-03-17 22:09:17 +0200

    fix start

--- src/components/CurrentCurrency/CurrentCurrency.jsx
+++ src/components/CurrentCurrency/CurrentCurrency.jsx
@@ -1,19 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-//import { Test } from './CurrentCurrency.styles';
-
-const CurrentCurrency = (props) => (
-  <div className="CurrentCurrencyWrapper">
-    Test content
-  </div>
+const CurrentCurrency = props => (
+  <div className="CurrentCurrencyWrapper">Test content</div>
 );
-
-CurrentCurrency.propTypes = {
-  // bla: PropTypes.string,
-};
-
-CurrentCurrency.defaultProps = {
-  // bla: 'test',
-};
 
 export default CurrentCurrency;
